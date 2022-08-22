9f2d67726ffcf78c5db186f20f2bba8c835068ba
juancarlosj01 <juancarlosj01@hotmail.com>
2022-08-22 18:29:20 -0400

    start

--- src/components/BackgroundTop.jsx
+++ src/components/BackgroundTop.jsx
@@ -1,17 +1,21 @@
 import { Button } from "@mui/material";
 import React from "react";
+import programando from "../assets/programando.mp4";
 
 const BackgroundTop = () => {
   return (
     <section>
       {/* <video src="https://vimeo.com/741324237"></video> */}
 
-      <video width="750" height="500" controls>
+      {/* <video width="750" height="500" controls>
         <source
           src="https://vimeo.com/741324237"
           type="video/mp4"
         />
       </video>
+ */}
+
+      <video src={programando} autoPlay loop muted />
 
       <div>
         <Button className="descargar" href="./src/assets/cv.pdf">
