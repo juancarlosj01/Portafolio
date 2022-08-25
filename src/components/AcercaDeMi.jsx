279aad3f6063be2589b63f954cd0cc238b996002
juancarlosj01 <juancarlosj01@hotmail.com>
2022-08-25 03:01:45 -0400

    fix

--- src/components/AcercaDeMi.jsx
+++ src/components/AcercaDeMi.jsx
@@ -3,27 +3,25 @@ import React from "react";
 
 const AcercaDeMi = () => {
   return (
-
-    <section id="about-me" >
-      
-
-<Grid
-  container
-  direction="row"
-  justifyContent="center"
-  alignItems="center"
->
-        <h2 className="title"  style={{marginTop: "1in"}}>Perfil</h2>
-    
-    <div className='descripcion'>
-        <h2>
-          Desarrollador web con 6 meses de experiencia, maquetado web,
-          proactivo, trabajo en equipo, Autorregulación del trabajo, Orientación
-          a resultados, Comunicación efectiva
+    <section id="about-me">
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <h2 className="title" style={{ marginTop: "1in" }}>
+          Perfil
         </h2>
-    </div>
-      
-</Grid>
+
+        <div className="descripcion">
+          <h2>
+            Desarrollador web, diseño y maquetado web, proactivo, trabajo en
+            equipo, Autorregulación del trabajo, Orientación a resultados,
+            Comunicación efectiva
+          </h2>
+        </div>
+      </Grid>
     </section>
   );
 };
