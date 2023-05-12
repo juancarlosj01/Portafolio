cbb4aca157cec9bf9b44de0e8499c4e811a65600
juancarlosj01 <juancarlosj01@hotmail.com>
2023-05-11 23:59:57 -0400

    agregar 3d

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import Skills from "./components/Skills";
 import Portafolio from "./components/Portafolio";
 import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
-
+import Portafolio2 from "./components/Portafolio2";
 
 function App() {
-
   return (
     <div className="general container">
-      
-
       <ResponsiveAppBar />
       <BackgroundTop />
       <AcercaDeMi />
       <Skills />
-      <Portafolio />
+      <div className="cajon">
+        <div className="cajon1">
+          <Portafolio />
+        </div>
+        <div className="cajon2">
+          <Portafolio2 />
+        </div>
+      </div>
       <ContactForm />
       <Footer />
-        
     </div>
   );
 }
--- src/components/Portafolio2.jsx
+++ src/components/Portafolio2.jsx
@@ -8,7 +8,6 @@ import {
 } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Grid } from "@mui/material";
-import { red } from "@mui/material/colors";
 
 const items = [
   /* {
@@ -17,49 +16,23 @@ const items = [
     caption: "Weather App",
     url: "https://helpful-snickerdoodle-1501e3.netlify.app/"
   },
-  
-  {
-    src: "https://i.ibb.co/HVsypSQ/Screenshot-2022-08-22-at-10-22-20-Ecommerce.png",
-    url: "https://regal-sawine-07f6e2.netlify.app/",
-    caption: "Tienda E-commerce",
-  },
-
-
-
-<a href="https://ibb.co/fDg55CQ"><img src="https://i.ibb.co/z6MYYSN/Screenshot-2023-03-19-at-20-53-55-JEAP-S-A-Consultor-a-Financiera-JEAP-S-A-Consultor-a-Financiera.png" alt="Screenshot-2023-03-19-at-20-53-55-JEAP-S-A-Consultor-a-Financiera-JEAP-S-A-Consultor-a-Financiera" border="0"></a>
-<a href="https://ibb.co/wBMQ6g8"><img src="https://i.ibb.co/MZM1NSK/Screenshot-2023-04-24-at-19-29-35-El-Rinc-n-de-las-cejas.png" alt="Screenshot-2023-04-24-at-19-29-35-El-Rinc-n-de-las-cejas" border="0"></a>
-<a href="https://ibb.co/k49jXnd"><img src="https://i.ibb.co/y6YtqT3/Screenshot-2023-04-24-at-19-33-17-Pantonebranding.png" alt="Screenshot-2023-04-24-at-19-33-17-Pantonebranding" border="0"></a>
-<a href="https://ibb.co/q0tYw3t"><img src="https://i.ibb.co/ZHnzCpn/Screenshot-2023-04-24-at-19-35-36-Bellavista-del-Pilar.png" alt="Screenshot-2023-04-24-at-19-35-36-Bellavista-del-Pilar" border="0"></a>
   */
 
+  //   <a href="https://ibb.co/hcgTLhM"><img src="https://i.ibb.co/QkJxpzc/espada.png" alt="espada" border="0"></a>
+  // <a href="https://ibb.co/9WTZbrm"><img src="https://i.ibb.co/m4qN9cr/Mausoleo.png" alt="Mausoleo" border="0"></a>
+  // <a href="https://ibb.co/ZcfFps5"><img src="https://i.ibb.co/8PmfL3q/mesa.jpg" alt="mesa" border="0"></a>
+  // <a href="https://ibb.co/pyDkfjJ"><img src="https://i.ibb.co/sHSXK9y/cocina1.jpg" alt="cocina1" border="0"></a>
+  // <a href="https://ibb.co/MGqT9LC"><img src="https://i.ibb.co/mXQsS7c/Dados.png" alt="Dados" border="0"></a>
+  // <a href="https://ibb.co/DQczLvP"><img src="https://i.ibb.co/KW4bmfc/escritorio.jpg" alt="escritorio" border="0"></a>
+
   {
-    src: "https://i.ibb.co/y6YtqT3/Screenshot-2023-04-24-at-19-33-17-Pantonebranding.png",
-    url: "https://pantonebranding.com/",
-    caption: "Pantone Branding",
-  },
-  {
-    src: "https://i.ibb.co/MZM1NSK/Screenshot-2023-04-24-at-19-29-35-El-Rinc-n-de-las-cejas.png",
-    url: "https://www.elrincondelascejas.com.py/",
-    caption: "El Rincón de las Cejas",
-  },
-  {
-    src: "https://i.ibb.co/z6MYYSN/Screenshot-2023-03-19-at-20-53-55-JEAP-S-A-Consultor-a-Financiera-JEAP-S-A-Consultor-a-Financiera.png",
-    url: "https://jeap.biz/",
-    caption: "Jeap Consultoría Financiera",
-  },
-  {
-    src: "https://i.ibb.co/ZHnzCpn/Screenshot-2023-04-24-at-19-35-36-Bellavista-del-Pilar.png",
-    url: "https://terrenosenparaguay.com/",
-    caption: "Bellavista del Pilar",
-  },
-  {
-    src: "https://i.ibb.co/JHW64vT/Screenshot-2022-08-22-at-10-29-00-Pokedex.png",
-    url: "https://golden-creponne-05da0a.netlify.app/",
-    caption: "Pokedex",
+    src: "https://i.ibb.co/KW4bmfc/escritorio.jpg",
+    url: "https://ibb.co/DQczLvP",
+    caption: "Escritorio Minimalista. Blender",
   },
 ];
 
-class Portafolio extends Component {
+class Portafolio2 extends Component {
   constructor(props) {
     super(props);
     this.state = { activeIndex: 0 };
@@ -136,13 +109,8 @@ class Portafolio extends Component {
           justifyContent="center"
           alignItems="center"
         >
-          <h3
-            className="title"
-            style={{
-              marginTop: "1in",
-            }}
-          >
-            Desarrollo Web
+          <h3 className="title" style={{ marginTop: "1in" }}>
+            Modelado / Animación 3D
           </h3>
         </Grid>
 
@@ -175,4 +143,4 @@ class Portafolio extends Component {
   }
 }
 
-export default Portafolio;
+export default Portafolio2;
